50bba2bb4258b9a32523575e55efd51e88b38b4f
Jeenia Sarkar <akunahjeenia@gmail.com>
2026-03-18 14:52:00 +0530

    Updated till Config File Tutorial

--- playwright.config.js
+++ playwright.config.js
@@ -1,33 +1,13 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
-import { TIMEOUT } from 'node:dns';
+const{defineConfig, chromium}=require('@playwright/test');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
-const config=({
-  testDir: './tests',
-  timeout:40*1000,
-
-  expect: {
-    timeout:40*1000,
-  },
-  
-  use:{
-    browserName:'chromium',
-  },
-  reporter:'html'
-
-
-});
-
-
-
+const config=defineConfig({
+testDir:'./tests',
+timeout:40*1000,
+use:{
+  headless:false,
+  browserName:'chromium'
+},
+reporter:'html'
+})
+module.exports = config;
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -9,7 +9,10 @@ const {test}= require ('@playwright/test');
 // });
 
 //REtrive two objects "Browser" and "Page" from Playwright default..............
-test('Open Google Two',async({browser,page})=>
+test('Open Rahul Shetty Academy',async({browser,page})=>
 {
 await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+})
+test.only('Open Google', async({page})=>{
+await  page.goto('https://www.google.com/');
 })
